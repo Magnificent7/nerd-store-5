b7ec1cdc8b3f3700802326d26fc2f5a06956a3dc
dani <dani.zaghian@gmail.com>
2017-05-17 21:31:43 -0700

    new orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+
+  def create
+    product_id = params[:product_id]
+    quantity = params[:quantity]
+    product = Product.find_by(id: product_id)
+    calculated_subtotal = product.price.to_i * quantity
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    order = Order.create(
+      user_id: current_user.id,
+      product_id: product_id,
+      quantity: quantity,
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+      )
+    flash[:success] = "Order Successfully Made!"
+    redirect_to "/orders/#{order.id}"
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+  end
+end
